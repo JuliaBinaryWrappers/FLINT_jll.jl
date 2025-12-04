c070eaf28d70ef27483af257a72a4821ecd7b63f
jlbuild <juliabuildbot@gmail.com>
2025-12-04 11:00:06 +0000

    FLINT_jll build 301.400.1+0

--- src/FLINT_jll.jl
+++ src/FLINT_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("FLINT")
-JLLWrappers.@generate_main_file("FLINT", UUID("e134572f-a0d5-539d-bddf-3cad8db41a82"))
+JLLWrappers.@generate_main_file("FLINT", Base.UUID("e134572f-a0d5-539d-bddf-3cad8db41a82"))
 end  # module FLINT_jll
